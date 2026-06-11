NewAmorend/AlphaGen-Agent

Protect GUI secrets from autofill pollution

src/wq_agent/gui/static/app.js
@@ -22,6 +22,13 @@ const PROVIDER_SECRET_KEYS = {
   kimi: "KIMI_API_KEY",
   deepseek: "DEEPSEEK_API_KEY",
 };
+const SECRET_AUTOCOMPLETE = {
+  OPENAI_API_KEY: "off",
+  KIMI_API_KEY: "off",
+  DEEPSEEK_API_KEY: "off",
+  EMBEDDING_API_KEY: "off",
+  WQ_PASSWORD: "new-password",
+};
 
 const $ = (id) => document.getElementById(id);
 
@@ -250,9 +257,28 @@ function configField(field) {
   if (field.secret) {
     const wrap = document.createElement("div");
     wrap.className = "secret-wrap";
+    const edit = document.createElement("button");
+    edit.type = "button";
+    edit.textContent = field.has_value ? "修改" : "设置";
+    edit.addEventListener("click", () => {
+      const editing = input.dataset.secretEditing === "true";
+      input.dataset.secretEditing = editing ? "false" : "true";
+      input.disabled = editing;
+      input.type = "password";
+      input.value = "";
+      input.placeholder = secretPlaceholder(field, !editing);
+      reveal.disabled = editing;
+      reveal.textContent = "显示";
+      edit.textContent = editing ? (field.has_value ? "修改" : "设置") : "取消";
+      refreshConfigStatusFromForm();
+      if (!editing) {
+        input.focus();
+      }
+    });
     const reveal = document.createElement("button");
     reveal.type = "button";
     reveal.textContent = "显示";
+    reveal.disabled = true;
     reveal.addEventListener("click", () => {
       input.type = input.type === "password" ? "text" : "password";
       reveal.textContent = input.type === "password" ? "显示" : "隐藏";
@@ -264,13 +290,20 @@ function configField(field) {
     clear.addEventListener("click", () => {
       const active = input.dataset.clearSecret !== "true";
       input.dataset.clearSecret = active ? "true" : "false";
-      input.disabled = active;
-      reveal.disabled = active;
+      input.disabled = true;
+      reveal.disabled = true;
+      edit.disabled = active;
+      input.dataset.secretEditing = "false";
+      input.type = "password";
+      input.value = "";
+      input.placeholder = secretPlaceholder(field, false);
+      reveal.textContent = "显示";
       clear.textContent = active ? "保留" : "清除";
       clear.classList.toggle("danger-inline", active);
       refreshConfigStatusFromForm();
     });
     wrap.appendChild(input);
+    wrap.appendChild(edit);
     wrap.appendChild(reveal);
     wrap.appendChild(clear);
     label.appendChild(wrap);
@@ -304,8 +337,17 @@ function createConfigInput(field) {
 
   const input = document.createElement("input");
   input.value = field.secret ? "" : field.value || "";
-  input.placeholder = field.secret && field.has_value ? "已设置，留空保持不变" : "";
+  input.placeholder = field.secret ? secretPlaceholder(field, false) : "";
   input.type = field.secret ? "password" : field.kind === "number" ? "number" : "text";
+  input.id = `config-${cssToken(field.key)}`;
+  input.name = `wq-agent-${cssToken(field.key)}`;
+  input.autocomplete = autocompleteForField(field);
+  if (field.secret) {
+    input.disabled = true;
+    input.dataset.secret = "true";
+    input.dataset.secretEditing = "false";
+    input.dataset.clearSecret = "false";
+  }
   let datalist = null;
   if (field.kind === "select" && field.allow_custom) {
     const listId = `options-${field.key}`;
@@ -325,6 +367,23 @@ function createConfigInput(field) {
   return { input, datalist };
 }
 
+function secretPlaceholder(field, editing) {
+  if (editing) {
+    return "输入新值后保存";
+  }
+  return field.has_value ? "已设置，点击修改后输入新值" : "未设置，点击设置后输入";
+}
+
+function autocompleteForField(field) {
+  if (field.key === "WQ_USERNAME") {
+    return "username";
+  }
+  if (field.secret) {
+    return SECRET_AUTOCOMPLETE[field.key] || "new-password";
+  }
+  return "off";
+}
+
 function handleConfigInputChange(event) {
   if (event.target.dataset.configKey === "LLM_PROVIDER") {
     state.configFields = currentConfigFieldsFromForm();
@@ -336,17 +395,7 @@ function handleConfigInputChange(event) {
 }
 
 async function saveConfig() {
-  const values = {};
-  document.querySelectorAll("[data-config-key]").forEach((input) => {
-    const key = input.dataset.configKey;
-    if (input.dataset.clearSecret === "true") {
-      values[key] = CLEAR_SECRET_VALUE;
-    } else if (input.type === "checkbox") {
-      values[key] = input.checked;
-    } else {
-      values[key] = input.value;
-    }
-  });
+  const values = collectConfigValues();
 
   try {
     const data = await api("/api/config", {
@@ -360,6 +409,26 @@ async function saveConfig() {
   } catch (error) {
     toast(error.message);
   }
+}
+
+function collectConfigValues() {
+  const values = {};
+  document.querySelectorAll("[data-config-key]").forEach((input) => {
+    const key = input.dataset.configKey;
+    const isSecret = input.dataset.secret === "true";
+    if (input.dataset.clearSecret === "true") {
+      values[key] = CLEAR_SECRET_VALUE;
+    } else if (isSecret && input.dataset.secretEditing !== "true") {
+      return;
+    } else if (isSecret && !input.value) {
+      return;
+    } else if (input.type === "checkbox") {
+      values[key] = input.checked;
+    } else {
+      values[key] = input.value;
+    }
+  });
+  return values;
 }
 
 function cssToken(value) {
@@ -410,11 +479,10 @@ function currentConfigFieldsFromForm() {
       if (input.dataset.clearSecret === "true") {
         next.has_value = false;
         next.value = "";
-      } else {
+      } else if (input.dataset.secretEditing === "true") {
         next.has_value = Boolean(input.value) || Boolean(field.has_value);
-        if (input.value) {
-          next.value = input.value;
-        }
+      } else {
+        next.has_value = Boolean(field.has_value);
       }
     } else if (input.type === "checkbox") {
       next.value = String(input.checked);

tests/test_gui.py
@@ -5,6 +5,8 @@ import io
 import json
 import os
 from pathlib import Path
+import shutil
+import subprocess
 import sys
 import threading
 import time
@@ -212,6 +214,102 @@ def test_env_save_can_clear_secret_explicitly(tmp_path):
     assert values["OPENAI_API_KEY"]["has_value"] is False
 
 
+def test_env_save_updates_wq_password_without_touching_api_keys(tmp_path):
+    (tmp_path / ".env").write_text(
+        "\n".join(
+            [
+                "OPENAI_API_KEY=openai-secret",
+                "KIMI_API_KEY=kimi-secret",
+                "DEEPSEEK_API_KEY=deepseek-secret",
+                "WQ_PASSWORD=old-wq-secret",
+            ]
+        )
+        + "\n",
+        encoding="utf-8",
+    )
+
+    EnvManager(tmp_path).save({"WQ_PASSWORD": "new-wq-secret"})
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+
+    assert "WQ_PASSWORD=new-wq-secret" in text
+    assert "OPENAI_API_KEY=openai-secret" in text
+    assert "KIMI_API_KEY=kimi-secret" in text
+    assert "DEEPSEEK_API_KEY=deepseek-secret" in text
+
+
+def test_env_save_updates_api_key_without_touching_wq_password(tmp_path):
+    (tmp_path / ".env").write_text(
+        "OPENAI_API_KEY=old-openai-secret\nWQ_PASSWORD=wq-secret\n",
+        encoding="utf-8",
+    )
+
+    EnvManager(tmp_path).save({"OPENAI_API_KEY": "new-openai-secret"})
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+
+    assert "OPENAI_API_KEY=new-openai-secret" in text
+    assert "WQ_PASSWORD=wq-secret" in text
+
+
+def test_env_save_rejects_matching_wq_password_and_api_key_updates(tmp_path):
+    (tmp_path / ".env").write_text(
+        "OPENAI_API_KEY=old-openai-secret\nWQ_PASSWORD=old-wq-secret\n",
+        encoding="utf-8",
+    )
+
+    with pytest.raises(ValueError, match="自动填充污染"):
+        EnvManager(tmp_path).save(
+            {
+                "OPENAI_API_KEY": "same-secret-value",
+                "WQ_PASSWORD": "same-secret-value",
+            }
+        )
+
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+    assert "OPENAI_API_KEY=old-openai-secret" in text
+    assert "WQ_PASSWORD=old-wq-secret" in text
+
+
+def test_env_save_rejects_api_key_matching_current_wq_password(tmp_path):
+    (tmp_path / ".env").write_text(
+        "OPENAI_API_KEY=old-openai-secret\nWQ_PASSWORD=current-wq-secret\n",
+        encoding="utf-8",
+    )
+
+    with pytest.raises(ValueError, match="自动填充污染"):
+        EnvManager(tmp_path).save({"OPENAI_API_KEY": "current-wq-secret"})
+
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+    assert "OPENAI_API_KEY=old-openai-secret" in text
+    assert "WQ_PASSWORD=current-wq-secret" in text
+
+
+def test_env_save_rejects_wq_password_matching_current_api_key(tmp_path):
+    (tmp_path / ".env").write_text(
+        "OPENAI_API_KEY=current-openai-secret\nWQ_PASSWORD=old-wq-secret\n",
+        encoding="utf-8",
+    )
+
+    with pytest.raises(ValueError, match="自动填充污染"):
+        EnvManager(tmp_path).save({"WQ_PASSWORD": "current-openai-secret"})
+
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+    assert "OPENAI_API_KEY=current-openai-secret" in text
+    assert "WQ_PASSWORD=old-wq-secret" in text
+
+
+def test_env_save_clear_one_secret_only_affects_that_key(tmp_path):
+    (tmp_path / ".env").write_text(
+        "OPENAI_API_KEY=openai-secret\nWQ_PASSWORD=wq-secret\n",
+        encoding="utf-8",
+    )
+
+    EnvManager(tmp_path).save({"OPENAI_API_KEY": CLEAR_SECRET_VALUE})
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+
+    assert "OPENAI_API_KEY=" in text
+    assert "WQ_PASSWORD=wq-secret" in text
+
+
 def test_env_save_deduplicates_keys_and_quotes_special_values(tmp_path):
     (tmp_path / ".env").write_text(
         "OPENAI_MODEL=old\nOPENAI_MODEL=older\nOPENAI_BASE_URL=https://api.openai.com/v1\n",
@@ -337,6 +435,117 @@ def test_config_model_options_stay_in_sync_with_factory_and_frontend():
         assert provider in app_js
         for model in models:
             assert model in app_js
+
+
+def test_frontend_secrets_require_explicit_editing_before_save():
+    app_js = (STATIC_DIR / "app.js").read_text(encoding="utf-8")
+
+    assert 'input.dataset.secret = "true"' in app_js
+    assert 'input.dataset.secretEditing = "false"' in app_js
+    assert 'input.disabled = true' in app_js
+    assert 'isSecret && input.dataset.secretEditing !== "true"' in app_js
+    assert "return;" in app_js
+    assert "autocompleteForField" in app_js
+    assert "wq-agent-${cssToken(field.key)}" in app_js
+
+
+def test_frontend_collect_config_values_secret_payload_behavior(tmp_path):
+    node = shutil.which("node")
+    if not node:
+        pytest.skip("node is required for frontend behavior coverage")
+
+    script = tmp_path / "config_payload_test.js"
+    script.write_text(
+        r"""
+const assert = require("assert");
+const fs = require("fs");
+const vm = require("vm");
+
+let currentInputs = [];
+const context = {
+  document: {
+    addEventListener() {},
+    getElementById() { return null; },
+    querySelectorAll(selector) {
+      assert.strictEqual(selector, "[data-config-key]");
+      return currentInputs;
+    },
+  },
+  window: { setTimeout() {} },
+};
+vm.createContext(context);
+vm.runInContext(fs.readFileSync(process.argv[2], "utf8"), context, { filename: "app.js" });
+
+function collect() {
+  return JSON.parse(JSON.stringify(context.collectConfigValues()));
+}
+
+function secret(key, options = {}) {
+  return {
+    dataset: {
+      configKey: key,
+      secret: "true",
+      secretEditing: options.editing || "false",
+      clearSecret: options.clear || "false",
+    },
+    type: "password",
+    value: options.value || "",
+  };
+}
+
+function plain(key, value) {
+  return { dataset: { configKey: key }, type: "text", value };
+}
+
+function checkbox(key, checked) {
+  return { dataset: { configKey: key }, type: "checkbox", checked };
+}
+
+currentInputs = [
+  secret("OPENAI_API_KEY", { value: "autofilled-wq-password" }),
+  secret("WQ_PASSWORD", { editing: "true", value: "new-wq-password" }),
+  plain("OPENAI_MODEL", "gpt-5.5"),
+];
+assert.deepStrictEqual(collect(), {
+  WQ_PASSWORD: "new-wq-password",
+  OPENAI_MODEL: "gpt-5.5",
+});
+
+currentInputs = [
+  secret("OPENAI_API_KEY", { editing: "true", value: "new-openai-key" }),
+  secret("WQ_PASSWORD", { value: "autofilled-wq-password" }),
+];
+assert.deepStrictEqual(collect(), {
+  OPENAI_API_KEY: "new-openai-key",
+});
+
+currentInputs = [
+  secret("OPENAI_API_KEY", { clear: "true", value: "should-not-send" }),
+  secret("WQ_PASSWORD", { value: "should-not-send" }),
+];
+assert.deepStrictEqual(collect(), {
+  OPENAI_API_KEY: "__clear_secret__",
+});
+
+currentInputs = [
+  secret("OPENAI_API_KEY", { editing: "true", value: "" }),
+  checkbox("OPENAI_STORE", true),
+];
+assert.deepStrictEqual(collect(), {
+  OPENAI_STORE: true,
+});
+""",
+        encoding="utf-8",
+    )
+
+    result = subprocess.run(
+        [node, str(script), str(STATIC_DIR / "app.js")],
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+
+    assert result.returncode == 0, result.stderr + result.stdout
 
 
 def test_build_cli_command_for_generate_and_backtest():

MakerFlyFly/wq-agent-public

src/wq_agent/gui/static/app.js
@@ -7,6 +7,12 @@ const state = {
   wikiUploading: false,
 };
 const CLEAR_SECRET_VALUE = "__clear_secret__";
+const SECRET_AUTOCOMPLETE = {
+  LLM_API_KEY: "off",
+  EMBEDDING_API_KEY: "off",
+  WQ_PASSWORD: "new-password",
+};
+
 const $ = (id) => document.getElementById(id);
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -198,9 +204,28 @@ function configField(field) {
   if (field.secret) {
     const wrap = document.createElement("div");
     wrap.className = "secret-wrap";
+    const edit = document.createElement("button");
+    edit.type = "button";
+    edit.textContent = field.has_value ? "修改" : "设置";
+    edit.addEventListener("click", () => {
+      const editing = input.dataset.secretEditing === "true";
+      input.dataset.secretEditing = editing ? "false" : "true";
+      input.disabled = editing;
+      input.type = "password";
+      input.value = "";
+      input.placeholder = secretPlaceholder(field, !editing);
+      reveal.disabled = editing;
+      reveal.textContent = "显示";
+      edit.textContent = editing ? (field.has_value ? "修改" : "设置") : "取消";
+      refreshConfigStatusFromForm();
+      if (!editing) {
+        input.focus();
+      }
+    });
     const reveal = document.createElement("button");
     reveal.type = "button";
     reveal.textContent = "显示";
+    reveal.disabled = true;
     reveal.addEventListener("click", () => {
       input.type = input.type === "password" ? "text" : "password";
       reveal.textContent = input.type === "password" ? "显示" : "隐藏";
@@ -212,13 +237,20 @@ function configField(field) {
     clear.addEventListener("click", () => {
       const active = input.dataset.clearSecret !== "true";
       input.dataset.clearSecret = active ? "true" : "false";
-      input.disabled = active;
-      reveal.disabled = active;
+      input.disabled = true;
+      reveal.disabled = true;
+      edit.disabled = active;
+      input.dataset.secretEditing = "false";
+      input.type = "password";
+      input.value = "";
+      input.placeholder = secretPlaceholder(field, false);
+      reveal.textContent = "显示";
       clear.textContent = active ? "保留" : "清除";
       clear.classList.toggle("danger-inline", active);
       refreshConfigStatusFromForm();
     });
     wrap.appendChild(input);
+    wrap.appendChild(edit);
     wrap.appendChild(reveal);
     wrap.appendChild(clear);
     label.appendChild(wrap);
@@ -252,8 +284,17 @@ function createConfigInput(field) {
 
   const input = document.createElement("input");
   input.value = field.secret ? "" : field.value || "";
-  input.placeholder = field.secret && field.has_value ? "已设置，留空保持不变" : "";
+  input.placeholder = field.secret ? secretPlaceholder(field, false) : "";
   input.type = field.secret ? "password" : field.kind === "number" ? "number" : "text";
+  input.id = `config-${cssToken(field.key)}`;
+  input.name = `wq-agent-${cssToken(field.key)}`;
+  input.autocomplete = autocompleteForField(field);
+  if (field.secret) {
+    input.disabled = true;
+    input.dataset.secret = "true";
+    input.dataset.secretEditing = "false";
+    input.dataset.clearSecret = "false";
+  }
   let datalist = null;
   if (field.kind === "select" && field.allow_custom) {
     const listId = `options-${field.key}`;
@@ -273,28 +314,29 @@ function createConfigInput(field) {
   return { input, datalist };
 }
 
-function handleConfigInputChange(event) {
-  if (event.target.dataset.configKey === "LLM_PROVIDER") {
-    state.configFields = currentConfigFieldsFromForm();
-    renderConfig(state.configFields);
-    renderConfigStatus(state.configFields);
-    return;
+function secretPlaceholder(field, editing) {
+  if (editing) {
+    return "输入新值后保存";
   }
+  return field.has_value ? "已设置，点击修改后输入新值" : "未设置，点击设置后输入";
+}
+
+function autocompleteForField(field) {
+  if (field.key === "WQ_USERNAME") {
+    return "username";
+  }
+  if (field.secret) {
+    return SECRET_AUTOCOMPLETE[field.key] || "new-password";
+  }
+  return "off";
+}
+
+function handleConfigInputChange() {
   refreshConfigStatusFromForm();
 }
 
 async function saveConfig() {
-  const values = {};
-  document.querySelectorAll("[data-config-key]").forEach((input) => {
-    const key = input.dataset.configKey;
-    if (input.dataset.clearSecret === "true") {
-      values[key] = CLEAR_SECRET_VALUE;
-    } else if (input.type === "checkbox") {
-      values[key] = input.checked;
-    } else {
-      values[key] = input.value;
-    }
-  });
+  const values = collectConfigValues();
 
   try {
     const data = await api("/api/config", {
@@ -308,6 +350,26 @@ async function saveConfig() {
   } catch (error) {
     toast(error.message);
   }
+}
+
+function collectConfigValues() {
+  const values = {};
+  document.querySelectorAll("[data-config-key]").forEach((input) => {
+    const key = input.dataset.configKey;
+    const isSecret = input.dataset.secret === "true";
+    if (input.dataset.clearSecret === "true") {
+      values[key] = CLEAR_SECRET_VALUE;
+    } else if (isSecret && input.dataset.secretEditing !== "true") {
+      return;
+    } else if (isSecret && !input.value) {
+      return;
+    } else if (input.type === "checkbox") {
+      values[key] = input.checked;
+    } else {
+      values[key] = input.value;
+    }
+  });
+  return values;
 }
 
 function cssToken(value) {
@@ -351,11 +413,10 @@ function currentConfigFieldsFromForm() {
       if (input.dataset.clearSecret === "true") {
         next.has_value = false;
         next.value = "";
-      } else {
+      } else if (input.dataset.secretEditing === "true") {
         next.has_value = Boolean(input.value) || Boolean(field.has_value);
-        if (input.value) {
-          next.value = input.value;
-        }
+      } else {
+        next.has_value = Boolean(field.has_value);
       }
     } else if (input.type === "checkbox") {
       next.value = String(input.checked);

tests/test_gui.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+﻿from __future__ import annotations
 
 import http.client
 import io
@@ -212,6 +212,100 @@ def test_env_save_can_clear_secret_explicitly(tmp_path):
     assert values["LLM_API_KEY"]["has_value"] is False
 
 
+def test_env_save_updates_wq_password_without_touching_api_keys(tmp_path):
+    (tmp_path / ".env").write_text(
+        "\n".join(
+            [
+                "LLM_API_KEY=llm-secret",
+                "UNRELATED_API_KEY=external-secret",
+                "WQ_PASSWORD=old-wq-secret",
+            ]
+        )
+        + "\n",
+        encoding="utf-8",
+    )
+
+    EnvManager(tmp_path).save({"WQ_PASSWORD": "new-wq-secret"})
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+
+    assert "WQ_PASSWORD=new-wq-secret" in text
+    assert "LLM_API_KEY=llm-secret" in text
+    assert "UNRELATED_API_KEY=external-secret" in text
+
+
+def test_env_save_updates_protocol_api_key_without_touching_wq_password(tmp_path):
+    (tmp_path / ".env").write_text(
+        "LLM_API_KEY=old-llm-secret\nWQ_PASSWORD=wq-secret\n",
+        encoding="utf-8",
+    )
+
+    EnvManager(tmp_path).save({"LLM_API_KEY": "new-llm-secret"})
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+
+    assert "LLM_API_KEY=new-llm-secret" in text
+    assert "WQ_PASSWORD=wq-secret" in text
+
+
+def test_env_save_rejects_matching_wq_password_and_api_key_updates(tmp_path):
+    (tmp_path / ".env").write_text(
+        "LLM_API_KEY=old-llm-secret\nWQ_PASSWORD=old-wq-secret\n",
+        encoding="utf-8",
+    )
+
+    with pytest.raises(ValueError, match="自动填充污染"):
+        EnvManager(tmp_path).save(
+            {
+                "LLM_API_KEY": "same-secret-value",
+                "WQ_PASSWORD": "same-secret-value",
+            }
+        )
+
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+    assert "LLM_API_KEY=old-llm-secret" in text
+    assert "WQ_PASSWORD=old-wq-secret" in text
+
+
+def test_env_save_rejects_api_key_matching_current_wq_password(tmp_path):
+    (tmp_path / ".env").write_text(
+        "LLM_API_KEY=old-llm-secret\nWQ_PASSWORD=current-wq-secret\n",
+        encoding="utf-8",
+    )
+
+    with pytest.raises(ValueError, match="自动填充污染"):
+        EnvManager(tmp_path).save({"LLM_API_KEY": "current-wq-secret"})
+
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+    assert "LLM_API_KEY=old-llm-secret" in text
+    assert "WQ_PASSWORD=current-wq-secret" in text
+
+
+def test_env_save_rejects_wq_password_matching_current_api_key(tmp_path):
+    (tmp_path / ".env").write_text(
+        "LLM_API_KEY=current-llm-secret\nWQ_PASSWORD=old-wq-secret\n",
+        encoding="utf-8",
+    )
+
+    with pytest.raises(ValueError, match="自动填充污染"):
+        EnvManager(tmp_path).save({"WQ_PASSWORD": "current-llm-secret"})
+
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+    assert "LLM_API_KEY=current-llm-secret" in text
+    assert "WQ_PASSWORD=old-wq-secret" in text
+
+
+def test_env_save_clear_one_secret_only_affects_that_key(tmp_path):
+    (tmp_path / ".env").write_text(
+        "LLM_API_KEY=llm-secret\nWQ_PASSWORD=wq-secret\n",
+        encoding="utf-8",
+    )
+
+    EnvManager(tmp_path).save({"LLM_API_KEY": CLEAR_SECRET_VALUE})
+    text = (tmp_path / ".env").read_text(encoding="utf-8")
+
+    assert "LLM_API_KEY=" in text
+    assert "WQ_PASSWORD=wq-secret" in text
+
+
 def test_env_save_deduplicates_keys_and_quotes_special_values(tmp_path):
     (tmp_path / ".env").write_text(
         "LLM_MODEL=old\nLLM_MODEL=older\nLLM_BASE_URL=https://api.openai.com/v1\n",
@@ -329,6 +423,116 @@ assert.strictEqual(select.value, "openai_compatible");
 
     assert result.returncode == 0, result.stderr + result.stdout
 
+
+def test_frontend_secrets_require_explicit_editing_before_save():
+    app_js = (STATIC_DIR / "app.js").read_text(encoding="utf-8")
+
+    assert 'input.dataset.secret = "true"' in app_js
+    assert 'input.dataset.secretEditing = "false"' in app_js
+    assert 'input.disabled = true' in app_js
+    assert 'isSecret && input.dataset.secretEditing !== "true"' in app_js
+    assert "return;" in app_js
+    assert "autocompleteForField" in app_js
+    assert "wq-agent-${cssToken(field.key)}" in app_js
+
+
+def test_frontend_collect_config_values_secret_payload_behavior(tmp_path):
+    node = shutil.which("node")
+    if not node:
+        pytest.skip("node is required for frontend behavior coverage")
+
+    script = tmp_path / "config_payload_test.js"
+    script.write_text(
+        r"""
+const assert = require("assert");
+const fs = require("fs");
+const vm = require("vm");
+
+let currentInputs = [];
+const context = {
+  document: {
+    addEventListener() {},
+    getElementById() { return null; },
+    querySelectorAll(selector) {
+      assert.strictEqual(selector, "[data-config-key]");
+      return currentInputs;
+    },
+  },
+  window: { setTimeout() {} },
+};
+vm.createContext(context);
+vm.runInContext(fs.readFileSync(process.argv[2], "utf8"), context, { filename: "app.js" });
+
+function collect() {
+  return JSON.parse(JSON.stringify(context.collectConfigValues()));
+}
+
+function secret(key, options = {}) {
+  return {
+    dataset: {
+      configKey: key,
+      secret: "true",
+      secretEditing: options.editing || "false",
+      clearSecret: options.clear || "false",
+    },
+    type: "password",
+    value: options.value || "",
+  };
+}
+
+function plain(key, value) {
+  return { dataset: { configKey: key }, type: "text", value };
+}
+
+function checkbox(key, checked) {
+  return { dataset: { configKey: key }, type: "checkbox", checked };
+}
+
+currentInputs = [
+  secret("LLM_API_KEY", { value: "autofilled-wq-password" }),
+  secret("WQ_PASSWORD", { editing: "true", value: "new-wq-password" }),
+  plain("LLM_MODEL", "gpt-5.5"),
+];
+assert.deepStrictEqual(collect(), {
+  WQ_PASSWORD: "new-wq-password",
+  LLM_MODEL: "gpt-5.5",
+});
+
+currentInputs = [
+  secret("LLM_API_KEY", { editing: "true", value: "new-llm-key" }),
+  secret("WQ_PASSWORD", { value: "autofilled-wq-password" }),
+];
+assert.deepStrictEqual(collect(), {
+  LLM_API_KEY: "new-llm-key",
+});
+
+currentInputs = [
+  secret("LLM_API_KEY", { clear: "true", value: "should-not-send" }),
+  secret("WQ_PASSWORD", { value: "should-not-send" }),
+];
+assert.deepStrictEqual(collect(), {
+  LLM_API_KEY: "__clear_secret__",
+});
+
+currentInputs = [
+  secret("LLM_API_KEY", { editing: "true", value: "" }),
+  checkbox("LLM_STORE", true),
+];
+assert.deepStrictEqual(collect(), {
+  LLM_STORE: true,
+});
+""",
+        encoding="utf-8",
+    )
+
+    result = subprocess.run(
+        [node, str(script), str(STATIC_DIR / "app.js")],
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+
+    assert result.returncode == 0, result.stderr + result.stdout
 
 def test_build_cli_command_for_generate_and_backtest():
     generate = build_cli_command(
@@ -787,7 +991,7 @@ def test_http_config_and_wiki_use_workspace_root(tmp_path):
     workspace = tmp_path / "workspace"
     workspace.mkdir()
     (workspace / ".env").write_text(
-        "OPENAI_MODEL=gpt-workspace\nWIKI_DIR=./wiki\nWIKI_AUTO_RECORD_DIR=./private_wiki\n",
+        "LLM_MODEL=gpt-workspace\nWIKI_DIR=./wiki\nWIKI_AUTO_RECORD_DIR=./private_wiki\n",
         encoding="utf-8",
     )
     (workspace / "wiki").mkdir()
@@ -799,7 +1003,7 @@ def test_http_config_and_wiki_use_workspace_root(tmp_path):
     )
     dist_root = workspace / "dist" / "wq-agent"
     dist_root.mkdir(parents=True)
-    (dist_root / ".env").write_text("OPENAI_MODEL=gpt-dist\n", encoding="utf-8")
+    (dist_root / ".env").write_text("LLM_MODEL=gpt-dist\n", encoding="utf-8")
 
     state, server, thread = _start_test_server(workspace)
     try:
@@ -829,7 +1033,7 @@ def test_http_config_and_wiki_use_workspace_root(tmp_path):
             tree = json.loads(response.read().decode("utf-8"))
 
         assert config["env_path"] == str(workspace / ".env")
-        assert values["OPENAI_MODEL"] == "gpt-workspace"
+        assert values["LLM_MODEL"] == "gpt-workspace"
         assert tree["roots"]["private"]["path"] == str(workspace / "private_wiki")
         assert tree["roots"]["private"]["file_count"] == 1
         assert tree["roots"]["private"]["tree"]["children"][0]["name"] == "secret-alpha.md"
@@ -1007,3 +1211,4 @@ def test_static_assets_reject_path_traversal_and_send_security_headers(tmp_path)
             assert exc.code == 404
     finally:
         _stop_test_server(server, thread)
+